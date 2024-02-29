c7e356f081402ec2ab739e61933e8e084357ac29
Alex <71541578+eXtract44@users.noreply.github.com>
2024-02-29 15:57:47 +0100

    deutch

--- Internet.ino
+++ Internet.ino
@@ -62,72 +62,64 @@ void handleNewMessages(int numNewMessages) {
     String from_name = bot.messages[i].from_name;
 
     if (text == "/start") {
-      String welcome = "Добро пожаловать в кухню, " + from_name + ".\n";
-      welcome += "Используйте следуйщие команды.\n\n";
-      welcome += "/check_kitchen для проверики датчиков на кухне \n";
+      String welcome = "Willkommen in der Küche, " + from_name + ".\n";
+      welcome += "Verwenden Sie die folgenden Befehle.\n\n";
+      welcome += "/check_kitchen zur Überprüfung der Sensoren in der Küche. \n";
       bot.sendMessage(chat_id, welcome, "");
     }
     if (text == "/beep") {
       beep();
       beep();
     }
-    /*if(text == "/set_standby_time"){
-      bot.sendMessage(chat_id, "min value 0 sec max value 65000 sec \n ", "");
-        pir_delay
-    }*/
-    //bot.sendMessage(chat_id, "HELLO FROM ESP32", "");
-    ////
     if (text == "/check_kitchen") {
-      String message = "Температура в кухне составляет: " + String(dht.readTemperature()) + " ºC \n";
-      message += "Влажность в кухне составляет: " + String(dht.readHumidity()) + " % \n";
+      String message = "Die Temperatur in der Küche beträgt: " + String(dht.readTemperature()) + " ºC \n";
+      message += "Die Luftfeuchtigkeit in der Küche beträgt: " + String(dht.readHumidity()) + " % \n";
       switch (read_air_status()) {
         case AIR_SENSOR_STATUS_GOOD:
-          message += "Качество воздуха в кухне отличное \n";
+          message += "Die Luftqualität in der Küche ist ausgezeichnet. \n";
           break;
         case AIR_SENSOR_STATUS_OK:
-          message += "Качество воздуха в кухне хорошее \n";
+          message += "Die Luftqualität in der Küche ist gut. \n";
           break;
         case AIR_SENSOR_STATUS_NORMAL:
-          message += "Качество воздуха в кухне нормальное \n";
+          message += "Die Luftqualität in der Küche ist ok. \n";
           break;
         case AIR_SENSOR_STATUS_BAD:
-          message += "Качество воздуха в кухне плохое \n";
+          message += "Die Luftqualität in der Küche ist schlecht. \n";
           break;
         case AIR_SENSOR_STATUS_VERY_BAD:
-          message += "Качество воздуха в кухне ужасное/опасность! \n";
+          message += "Die Luftqualität in der Küche ist gefährlich! \n";
           break;
       }
       message += "MQ2 " + String(read_mq2()) + " ppm \n";
       message += "MQ135 " + String(read_mq135()) + " ppm \n";
       ////
       if (active_pir_sensor()) {
-        message += "В кухне кто-то есть \n";
+        message += "In der Küche ist jemand. \n";
       } else {
-        message += "В кухне никого нету \n";
+        message += "In der Küche ist niemand. \n";
       }
       ////
-      message += "Датчики протечки: \n";
-      message += "Под холодильником влажонсть состовляет: " + String(read_water_sensor_1()) + " % \n";
-      message += "Под стиральной машинкой влажонсть состовляет: " + String(read_water_sensor_2()) + " % \n";
-      message += "Под умывальником влажонсть состовляет: " + String(read_water_sensor_3()) + " % \n";
+      message += "Leckage-Sensoren: \n";
+      message += "Die Feuchtigkeit unter dem Kühlschrank beträgt: " + String(read_water_sensor_1()) + " % \n";
+      message += "Die Feuchtigkeit unter der Waschmaschine beträgt: "  + String(read_water_sensor_2()) + " % \n";
+      message += "Die Feuchtigkeit unter dem Waschbecken beträgt: "  + String(read_water_sensor_3()) + " % \n";
       bot.sendMessage(chat_id, message, "");
 
-    }  //// /check_kitchen
+    }
 
     if (read_water_sensor_3() > read_water_alarm()) {
-      bot.sendMessage(chat_id, "ВНИМАНИЕ ПРОТЕЧКА ПОД ХОЛОДИЛЬНИКОМ !!!", "");
+      bot.sendMessage(chat_id, "Achtung! Es gibt eine Undichtigkeit unter dem Kühlschrank!!!", "");
     }
     if (read_water_sensor_2() > read_water_alarm()) {
-      bot.sendMessage(chat_id, "ВНИМАНИЕ ПРОТЕЧКА ПОД СТИРАЛКОЙ !!!", "");
+      bot.sendMessage(chat_id, "Achtung! Es gibt eine Undichtigkeit unter der Waschmaschine!!!", "");
     }
     if (read_water_sensor_1() > read_water_alarm()) {
-      bot.sendMessage(chat_id, "ВНИМАНИЕ ПРОТЕЧКА ЗА МУСОРНИКОМ !!!", "");
+      bot.sendMessage(chat_id, "Achtung! Es gibt eine Undichtigkeit hinter dem Mülleimer!!!", "");
     }
     if (dht.readTemperature() > 35 || dht.readHumidity() > 85) {
-      bot.sendMessage(chat_id, "ВНИМАНИЕ ВЫСОКАЯ ТЕМПЕРАТУРА/ВЛАЖОНСТЬ В КУХНЕ !!!", "");
+      bot.sendMessage(chat_id, "Achtung! Hohe Temperatur/Feuchtigkeit in der Küche!!!", "");
     }
   }
-  //cnt = 0;
-  //}
 }
 #endif
